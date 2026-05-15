a9fca062bd89a64fca4f022f96fb4ea687fb96b8
Emily <welponere@hotmail.com>
2026-05-15 19:52:59 +0200

    Initial Commit

---
+++ TravelingFriends/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TravelingFriends
+//
+//  Created by Emily on 15/05/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TravelingFriendsTests/TravelingFriendsTests.swift
@@ -0,0 +1,18 @@
+//
+//  TravelingFriendsTests.swift
+//  TravelingFriendsTests
+//
+//  Created by Emily on 15/05/2026.
+//
+
+import Testing
+
+struct TravelingFriendsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
